19a4ba98e34f8f07ac2d1607d2dd30e4b98b6d74
Devil0monster <161365069+Devil0monster@users.noreply.github.com>
2024-02-27 10:35:00 +0000

    container

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId= 144553
+let accountEmail = "monster@gmail.com"
+var accountPassword =112233
+accountCity = "delhi"
+
+// accountId =2  // not allowed
+
+accountEmail = "hack@gmail.com"
+accountPassword = 12345678
+accountCity = "mumbai"
+console.table([accountId , accountEmail , accountPassword , accountCity])
+
+console.log(accountId);
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node js, not browser
+
+console.log(3+3);  // code readability should be high
+console.log("monster");
+
+let name = "monster"
+let age = 20
+let isloggedIn = false
+let state;
+let log = null
+
+
+// number => 2 ka power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol =>unique
+
+// object
+
+console.log(typeof undefined); // type undefined
+console.log(typeof null);  // type object
